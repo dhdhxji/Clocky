755e7aa5cf107c2af8d65600e1eae17981e2bdce
Oleksandr <keysindie@gmail.com>
2022-03-20 13:19:30 +0200

    fix: Fix linking with lua bindings

--- main/main.cpp
+++ main/main.cpp
@@ -1,26 +1,15 @@
-#include "colproc/colproc.h"
-#include "colproc/gen/rainbow.h"
-#include "colproc/gen/text.h"
-#include "colproc/mod/move.h"
-#include "colproc/mod/mixer.h"
-
-#include "colproc/filter/brigtness_scale.h"
-#include "colproc/variable/variable_constant.h"
+#include <iostream>
+#include <exception>
+#include <string>
+#include "colproc/runtime/lua_runtime.h"
 #include "colproc/variable/variable_callback.h"
-#include "colproc/switch/switch.h"
-#include "colproc/switch/transition_simple.h"
-#include "colproc/mod/concat.h"
-#include "colproc/runtime/runtime.h"
 
-#include <chrono>
 
 
 #define REFRESH_RATE_HZ 30
 #define MATRIX_W        19
 #define MATRIX_H        7
 
-
-using namespace std::chrono;
 
 
 class CanvasConsole: public Canvas
@@ -90,98 +79,36 @@ private:
 
 
 
-static ColProc* build_processor(IVariableStorage& storage) {
-    storage.addVariable(
-        "hrs",
-        new VariableCallback<std::string>([](){
-            time_t ttime = time(0);
-            struct tm* tm_time = localtime(&ttime);
-            return  std::to_string(tm_time->tm_hour);
-        })
-    );
-
-    storage.addVariable(
-        "mins",
-        new VariableCallback<std::string>([](){
-            time_t ttime = time(0);
-            struct tm* tm_time = localtime(&ttime);
-            return  std::to_string(tm_time->tm_min);
-        })
-    );
-
-    ColProc* hrs = new GeneratorText(
-        storage.getVariable("hrs")->castToVariable<std::string>(),
-        new VariableConstant<std::string>("3_by_57")
-    );
-
-    ColProc* dash = new Move(
-        new GeneratorText(
-            new VariableConstant<std::string>(":"),
-            new VariableConstant<std::string>("3_by_57")
-        ),
-        new VariableConstant<int32_t>(7),
-        new VariableConstant<int32_t>(0)
-    );
-
-    ColProc* min = new Move(
-        new GeneratorText(
-            storage.getVariable("mins")->castToVariable<std::string>(),
-            new VariableConstant<std::string>("3_by_57")
-        ),
-        new VariableConstant<int32_t>(10),
-        new VariableConstant<int32_t>(0)
-    );
-
-    ColProc* text = new Concat({hrs, dash, min});
-
-    ColProc* moved_text = new Move(
-        text,
-        new VariableConstant<int32_t>(1),
-        new VariableConstant<int32_t>(1)
-    );
-
-    ColProc* rainbow = new GeneratorRainbow(
-        new VariableConstant<int32_t>(50),
-        new VariableConstant<int32_t>(0),
-        new VariableConstant<int32_t>(5000)
-    );
-    
-    ColProc* mixed_text = new Mixer(rainbow, moved_text);
-
-    ColProc* switched = new Switch(
-        {mixed_text, rainbow},
-        storage.getVariable("rainbow_text_select")->castToVariable<int32_t>(),
-        new TransitionSimple()
-    );
-
-    return new BrightnessScale(
-        switched, 
-        new VariableConstant<double>(1)
-    );
-}
-
-
-extern "C" void app_main()
-{
+extern "C" void app_main() {
     CanvasConsole canvas(MATRIX_W, MATRIX_H);
-
-    Runtime rt;
-    rt.setCanvas(&canvas);
-    rt.setFrameRate(REFRESH_RATE_HZ);
     
-    rt.getVariableManager().addVariable(
-        "rainbow_text_select",
-        new VariableCallback<int32_t>(
-            []() {
-                int32_t time_ms = 
-                    duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-                return (time_ms/1000) % 2;
-            }
-        )
-    );
+    try {
+        LuaRuntime rt(&canvas, 30);
 
-    std::unique_ptr<ColProc> node( build_processor(rt.getVariableManager()) );
-    rt.setRenderNode(node.get());
+        rt.getVariableManager().addVariable(
+            "hrs",
+            new VariableCallback<std::string>([](){
+                time_t ttime = time(0);
+                struct tm* tm_time = localtime(&ttime);
+                return  std::to_string(tm_time->tm_hour);
+            })
+        );
 
-    rt.runRenderLoop();
+        rt.getVariableManager().addVariable(
+            "mins",
+            new VariableCallback<std::string>([](){
+                time_t ttime = time(0);
+                struct tm* tm_time = localtime(&ttime);
+                return  std::to_string(tm_time->tm_min);
+            })
+        );
+
+        rt.loadScript("../init.lua");
+
+        rt.runRenderLoop();
+    } catch(std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+    } catch(...) {
+        std::cerr << "Unexpected error" << std::endl;
+    }
 }
