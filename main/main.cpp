83b6dd40d3fd703f3771a93dce39d8522bb7f521
Oleksandr <keysindie@gmail.com>
2022-03-25 22:57:32 +0200

    ref: Move canvas implementation to separate header

--- main/main.cpp
+++ main/main.cpp
@@ -11,86 +11,14 @@
 #include "nvs_flash.h"
 #include "wifi_wrapper.hpp"
 #include "web_server.hpp"
+#include "canvas_impl.h"
 
 extern "C" {
 #include "file_server.h"
 }
 
-#define WIFI_SSID   ""
-#define WIFI_PASS   ""
-
-
-#define REFRESH_RATE_HZ 30
-#define MATRIX_W        19
-#define MATRIX_H        7
-
-
-
-class CanvasConsole: public Canvas
-{
-public:
-    CanvasConsole(size_t w, size_t h): Canvas(w, h) {
-        _canvas = new ColRGB[w*h];
-
-        for(size_t i = 0; i < w*h; ++i) {
-            _canvas[i].r = 0;
-            _canvas[i].g = 0;
-            _canvas[i].b = 0;
-        } 
-    }
-
-    ~CanvasConsole() override {
-        delete[] _canvas;
-    }
-
-    void setPix(size_t x, size_t y, ColRGB col) override {
-        if(x >= getW() || y >= getH()) {
-            throw std::runtime_error(
-                std::string("Out of bounds in setPix(") + 
-                std::to_string(x) + 
-                ";" + 
-                std::to_string(y) + 
-                ")"
-            );
-            return;
-        }
-        size_t index = y*getW() + x;
-
-        _canvas[index] = col;
-    }
-
-    ColRGB getPix(size_t x, size_t y) const override {
-        if(x >= getW() || y >= getH()) { 
-            throw std::runtime_error(
-                std::string("Out of bounds in getPix(") + 
-                std::to_string(x) + 
-                ";" + 
-                std::to_string(y) + 
-                ")"
-            );
-            return ColRGB(0,0,0);
-        }
-        size_t index = y*getW() + x;
-
-        return _canvas[index];
-    }
-
-    void display() const override {
-        printf("\033[0;0H");
-        for(size_t y = 0; y < getH(); ++y) {
-            for(size_t x = 0; x < getW(); ++x) {
-                ColRGB col = getPix(x, y);
-                printf("\033[48;2;%d;%d;%dm ", col.r, col.g, col.b);
-                printf("\033[48;2;%d;%d;%dm ", col.r, col.g, col.b);
-            }
-            printf("\n");
-        }
-    }
-
-private:
-    ColRGB* _canvas;
-};
-
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 void print_system_info() {
     esp_chip_info_t chip_info;
@@ -108,6 +36,7 @@ void print_system_info() {
 
     printf("Free heap: %d\n", esp_get_free_heap_size());
 }
+
 
 extern "C" void app_main() {
     print_system_info();
@@ -129,7 +58,7 @@ extern "C" void app_main() {
 
     vTaskDelay(portMAX_DELAY);
 
-    CanvasConsole canvas(MATRIX_W, MATRIX_H);
+    CanvasConsole canvas(MATRIX_W_DEFAULT, MATRIX_H_DEFAULT);
     try {
         LuaRuntime rt(&canvas, 30);
 
